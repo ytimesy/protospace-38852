ec5c3c2548768c01aa97f9db5685e832417790a4
ytimesy <yukio.juna1224@gmail.com>
2022-12-30 08:44:46 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,60 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user! ,except: [ :index, :show ]
+  before_action :move_to_index, except: [ :index, :show ]
+
+  def index
+    @prototypes = Prototype.includes(:user)
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+        render :new
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @prototype = Prototype.find(params[:id])
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])  
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])  
+    prototype.destroy
+    redirect_to root_path  
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end 
+
+  def move_to_index
+    unless Prototype.find(params[:id]).user == current_user
+      redirect_to action: :index
+    end
+  end
+
+
+end
