c9d02b4d0fab94477318c6decdff5603cd8b12d1
ytimesy <yukio.juna1224@gmail.com>
2022-12-30 11:48:52 +0900

    投稿画面解消その３

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user! ,only: :edit
+  before_action :authenticate_user! ,except: [ :index, :show ]
   before_action :move_to_index, except: [ :index, :show ]
 
   def index
